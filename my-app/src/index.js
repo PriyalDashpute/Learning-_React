e75d6f1c8e975f9cbdf31fce795ec07643dc7536
priyal <priyaldashpute1919@gmail.com>
2024-03-02 11:24:43 +0530

    first commit of dat one

---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+//const h1 = document.createElement('h1')
+//h1.innerHTML ='I am learning reactjs';
+
+//document.getElementById("root").appendChild(h1)
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+ReactDOM.render(
+  <React.Fragment>
+<h1>I am learning react js</h1>
+<h2>Today is day one</h2>
+<h2>Today is day one</h2>
+<h2>Today is day one</h2>
+</React.Fragment>,
+document.getElementById("root")
+);
